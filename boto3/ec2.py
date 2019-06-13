f02877af4279ff53aacef1c82b88c358b8083385
Gerry Smyth <gerry.p.smyth@gmail.com>
2019-06-13 20:49:06 +1200

    Using class

--- boto3/ec2.py
+++ boto3/ec2.py
@@ -1,7 +1,20 @@
+# pylint: disable=E1101
+""" Using boto3 methods """
 import boto3
 
+class InstanceToGet:
+    """ Class about Instances """
+    def __init__(self, name):
+        # pylint complained here
+        session = boto3.Session(profile_name=name)
+        self._ec2 = session.resource('ec2')
+
+    def list_instances(self):
+        """ list the instances """
+        for i in self._ec2.instances.all():
+            print(i)
+
 if __name__ == '__main__':
-    session = boto3.Session(profile_name="analyser")
-    ec2 = session.resource('ec2')
-    for i in ec2.instances.all():
-        print (i)
+    MYINSTANCES = InstanceToGet("analyser")
+    MYINSTANCES.list_instances()
+    
