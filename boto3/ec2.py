bca2bf3b844d8b41e6c5c76d0326139155aec96f
Gerry Smyth <gerry.p.smyth@gmail.com>
2019-06-14 19:34:23 +1200

    Incorporating decorator :ok_hand: and using click module

--- boto3/ec2.py
+++ boto3/ec2.py
@@ -1,6 +1,13 @@
 # pylint: disable=E1101
 """ Using boto3 methods """
+import click
 import boto3
+
+@click.command()
+def cli():
+    """ Understanding click decorator """
+    click.echo("Hello World")
+
 
 class InstanceToGet:
     """ Class about Instances """
@@ -12,9 +19,22 @@ class InstanceToGet:
     def list_instances(self):
         """ list the instances """
         for i in self._ec2.instances.all():
-            print(i)
+            try:
+                # i.id is the same as i.instance_id so no need to duplicate
+                print(', '.join((i.id,
+                                 i.instance_type,
+                                 i.placement['AvailabilityZone'],
+                                 i.state['Name'],
+                                 i.public_ip_address)))
+            except TypeError:
+                # how handle i.public_ip_address when None as in stopped
+                print(', '.join((i.id,
+                                 i.instance_type,
+                                 i.placement['AvailabilityZone'],
+                                 i.state['Name'])))
+
 
 if __name__ == '__main__':
     MYINSTANCES = InstanceToGet("analyser")
     MYINSTANCES.list_instances()
-    
+    cli()
